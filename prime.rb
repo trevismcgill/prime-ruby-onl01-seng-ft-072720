16f45b34d7e7cbf4172d40eb0fea1eb13b2db6a0
Trevis McGill <trevismcgill25@gmail.com>
2020-07-30 01:38:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(integer)
+  if integer > 1
+    range = (2..integer-1).to_a
+
+    range.none? do |int_to_test|
+  	integer % int_to_test == 0
+    end
+  else
+    return false
+  end
+end
